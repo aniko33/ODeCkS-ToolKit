c32e69cfea893675b99d1d3040b488e93e2a0f5a
Aniko <76649588+aniko33@users.noreply.github.com>
2022-03-01 21:22:47 +0100

    Update main.py

--- main.py
+++ main.py
@@ -163,7 +163,7 @@ def upgrade():
     [4] Phoneinfoga
     [5] Sherlock
     [6] Whois''')
-    install=input(Fore.LIGHTYELLOW_EX+"Select > ")
+    install=input("Select > ")
     if install=='1':
         os.system('sudo apt install nmap && sudo pacman -S nmap')
     elif install=='6':
